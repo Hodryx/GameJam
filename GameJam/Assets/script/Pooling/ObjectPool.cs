a8dc1a1575628e022fcd08e9410edb9cbad81ce8
benjamin_debosscher <benjamin.debosscher@student.vinci.be>
2019-11-15 15:10:51 +0100

    first texture to be blind

---
+++ GameJam/Assets/script/Pooling/ObjectPool.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool 
+{
+    List<Poolable> pool = new List<Poolable>();
+    Poolable prefab;
+
+    public ObjectPool(Poolable prefab) {
+        this.prefab = prefab;
+    }
+    /*
+     * Acquire fait que on utilise un object poolable
+     * si on ne l a pas on ajoute +1 element a la liste 
+     */
+    public Poolable Aquire() {
+        Poolable obj;
+        //si j ai un objet non utiliser dans la pool
+        //je le retire pour l'utiliser
+        if (pool.Count > 0)
+        {
+            obj = pool[pool.Count - 1];
+            pool.RemoveAt(pool.Count - 1);
+            obj.IsPooled = false;
+            return obj;
+        }
+        //si pool vide J instancie un prefab de poolable
+        obj = Object.Instantiate(prefab);
+        obj.IsPooled = false;
+        return obj;
+    }
+    /*
+     *remet l'element dans la poole si celui si n'est pas Pool 
+     */
+    public bool Release(Poolable obj) {
+        if (!obj.IsPooled) {
+            obj.IsPooled = true;
+            pool.Add(obj);
+            return true;
+        }
+        return false;    
+    }
+    /*
+     * retire de la pool l'objet Poolable
+     */
+    public void Remove(Poolable obj) {
+        if (obj.IsPooled) {
+            obj.IsPooled = false;
+            pool.Remove(obj);
+        }
+    }
+}
---
+++ GameJam/Assets/script/Pooling/Poolable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poolable : MonoBehaviour
+{
+    public ObjectPool pool;
+
+    bool isPooled;
+    bool isBeingDestroyed;
+
+    public bool IsPooled{
+        get {
+            return isPooled;
+        }
+        set {
+            isPooled = value;
+            if (!isBeingDestroyed)
+                gameObject.SetActive(!value);
+        }
+    }
+
+    public Poolable Acquire() {
+        if (ReferenceEquals(pool, null)) {
+            pool = new ObjectPool(this);
+        }
+        var obj = pool.Aquire();
+        if (ReferenceEquals(obj.pool, null))
+            obj.pool = this.pool;
+        return obj;
+    }
+
+    public bool Release() {
+        if (ReferenceEquals(pool, null)) {
+            return false;
+        }
+        return pool.Release(this);
+    }
+    public void voidReleaseImmediate() {
+        if (!ReferenceEquals(pool, null))
+        {
+            pool.Release(this);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    public void RemoveFromPool() {
+        if (pool != null) {
+            pool.Remove(this);
+            pool = null;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        isBeingDestroyed = true;
+        RemoveFromPool();
+    }
+}
